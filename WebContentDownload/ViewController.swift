9dc5d617367d0c9a1d778df6b3113b92ef3645db
soumyajitz <2014soumyajit@gmail.com>
2016-02-01 23:40:50 -0600

    Web Content Download
    Down Webcontent using NSURL() and NSURLSession.

--- WebContentDownload/ViewController.swift
+++ WebContentDownload/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
-
+var textVal = String()
 class ViewController: UIViewController {
+    
+    @IBOutlet var textbox: UITextField!
+    
+    @IBAction func go(sender: AnyObject) {
+        
+}
+    @IBOutlet var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // textVal = String(textbox.text!)
+        if textVal == "" {
+        
+        let url = NSURL(string:"https://www.apple.com")!
+        webView.loadRequest(NSURLRequest(URL: url))
+        }
+        else
+        {
+            webView.loadRequest(NSURLRequest(URL: NSURL(string: textVal)!))
+            webView.reload()
+
+        }
+        /*
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data,response,error) -> Void in
+            // Will happen when the task is completed
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                // Sometimes it takes a long time for the queue to finish
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                     self.webView.loadHTMLString(String(webContent!), baseURL: nil)
+                })
+            }
+            else{
+                // Show the error message
+                
+            }
+        }
+        
+        task.resume()
+*/
     }
 
     override func didReceiveMemoryWarning() {
